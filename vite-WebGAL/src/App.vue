ee47af4c8629f6a4e58163d0ccf25e1fb0cdb2d0
zhaizhe520 <zhaizhe520@gmail.com>
2026-05-10 11:38:16 +0800

    webgal

---
+++ vite-WebGAL/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import WebGal from './components/WebGal.vue';
+
+
+
+</script>
+
+<template>
+<WebGal />
+
+</template>
---
+++ vite-WebGAL/src/components/WebGal.vue
@@ -0,0 +1,10 @@
+<template>
+
+  <h1>这是webgal轻量模型</h1>
+
+
+
+  
+</template>
+
+
